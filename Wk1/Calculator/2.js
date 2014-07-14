70f567dd674bb1a67981c967b3a66d9e7ff55f62
tonyyzhu <tonyyzhu@tpg.com.au>
2014-07-15 00:52:04 +1000

    Finally a workable simple calculator! Abide too simple at the moment!

--- Wk1/Calculator/2.js
+++ Wk1/Calculator/2.js
@@ -2,21 +2,18 @@
 // Step 1: Building a function add()
 
 function add(a,b) {
-    result = a + b;
-    return result;
+    return a + b;
 };
 
 //Step 2: Build multiply, divide and subtract function
 
 function multiply(a, b) {
-    result = a * b;
-    return result;
+    return a * b;
 };
 
 function divide(a,b) {
     if (b != 0) {
-            result = a/b;
-            return result;
+            return a/b;
     }
     else{
             alert ("numbers can not be divided by zero!");
@@ -24,8 +21,7 @@ function divide(a,b) {
 };
 
 function subtract(a,b) {
-    result = a - b;
-    return result;
+    return a - b;
 };
 
 // Test above functions
@@ -42,30 +38,34 @@ $(function() {
         switch(st1) {
             case "Clear":
                 myDisplay.innerHTML = "#";
-                break;
+                st2 = "";
+                break;          // Return to initial status and then break out;
             case "=":
                 var arr0 = st2.split("=");
                 var st3 = arr0[0];
                 if (st3.indexOf("+") !== -1) {
                     var arr1 = st3.split("+");
                     myDisplay.innerHTML = add(parseFloat(arr1[0]), parseFloat(arr1[1]));
-                    break;
                 }
                 else if (st3.indexOf("-") !==-1) {
-                    var arr2 = st3.split("-");
-                    subtract(parseFloat(arr2[0]), parseFloat(arr2[1]));
-                    break;
+                    var arr1 = st3.split("-");
+                    myDisplay.innerHTML = subtract(parseFloat(arr1[0]), parseFloat(arr1[1]));
                 }
-                else if (st3.indexOf("*")) {
-                    var arr3 = st3.split("*");
-                    multiply(parseFloat(arr3[0]), parseFloat(arr3[1]));
-                    break;
+                else if (st3.indexOf("*") !==-1) {
+                    var arr1 = st3.split("*");
+                    myDisplay.innerHTML = multiply(parseFloat(arr1[0]), parseFloat(arr1[1]));
+                }
+                else if (st3.indexOf("/") !==-1) {
+                    var arr1 = st3.split("/");
+                    myDisplay.innerHTML = divide(parseFloat(arr1[0]), parseFloat(arr1[1]));
                 }
                 else {
-                    var arr4 = st3.split("/");
-                    divide(parseFloat(arr4[0]), parseFloat(arr4[1]));
-                    break;
+                    alert("nothing here!");
                 }
+                st2 = "";
+                arr0 = [];
+                arr1 = [];
+                break;         // Return to initial status and then break out;
         }
     });
 });
