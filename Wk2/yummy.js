b449442821219c3c889c211d57c94a795ad4bf67
tonyyzhu <tonyyzhu@tpg.com.au>
2014-07-21 17:14:45 +1000

    unfinished business

--- Wk2/yummy.js
+++ Wk2/yummy.js
@@ -1,7 +1,13 @@
 // Use JS and jQuery to add content to Web Page
 $("#content").prepend("<h1>Welcome to Seafood Restaurant!</h1>");
 $("h1").css("text-align", "center");
-$("#content").append();
+$("dish").click(function() {
+    $("img.#dish").toggle('slow');
+});
+$("table").onfocus = function() {
+    $("#image").append("<img src="./img/location2.jpg" align="absbottom" />");
+};
+//
 
 
 //$("li").css("border", "3px, solid, yellow");
