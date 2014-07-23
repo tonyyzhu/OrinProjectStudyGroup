2184a59630ad32a0de5eb00f87ad2358ca1c2561
Tony Zhu <tyz05@hotmail.com>
2014-07-24 03:08:39 +1000

    Just another unnecessary try

--- Wk2/yummy.js
+++ Wk2/yummy.js
@@ -1,7 +1,9 @@
 // Use JS and jQuery to add content to Web Page
 $("#content").prepend("<h1>Welcome to Seafood Restaurant!</h1>");
 $("h1").css("text-align", "center");
-$("th#dish").click() = function() {
+
+
+/*$("th#dish").click() = function() {
     $("img.#dish").toggle("show");
     $("img.#table").toggle("hide");
 };
@@ -12,7 +14,32 @@ $("th#table").click() = function() {
 };
 
 //$("li").css("border", "3px, solid, yellow");
-/*
+
 $("#content").append();
 $("#content").append();
 */
+
+/*
+    <div id="content">
+       <table class="collapse">
+            <caption><code>Menu</code></caption>
+            <tbody>
+               <tr>
+                    <th id="dish">Dish</th>
+                    <th id="table">Table</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div id="image">
+        <style type="text/css">
+            img {
+                margin-left:50px;
+                width:800px;
+                height:400px;
+                }
+        </style>
+        <img src="./img/dish4.jpg" align="absbottom" id="dish"/>
+        <img src="./img/location2.jpg" align="absbottom" id="table"/>
+    </div>
+    */
