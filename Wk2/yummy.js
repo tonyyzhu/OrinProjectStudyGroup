3a2146a06c1f058032058e682eecd9f0d9198ef0
tonyyzhu <tonyyzhu@tpg.com.au>
2014-07-23 16:41:08 +1000

    anotherTryNoGood

--- Wk2/yummy.js
+++ Wk2/yummy.js
@@ -1,15 +1,15 @@
 // Use JS and jQuery to add content to Web Page
 $("#content").prepend("<h1>Welcome to Seafood Restaurant!</h1>");
 $("h1").css("text-align", "center");
-$("th#dish").click() = function() {
+$("th#dish").click(function() {
     $("img.#dish").toggle("show");
     $("img.#table").toggle("hide");
-};
+});
 
-$("th#table").click() = function() {
+$("th#table").click(function() {
     $("img.#table").toggle("show");
     $("img.#dish").toggle("hide");
-};
+});
 
 //$("li").css("border", "3px, solid, yellow");
 /*
