a29856dfec0c85294bd058aecfaec82bf8499398
tonyyzhu <tonyyzhu@tpg.com.au>
2014-07-23 16:15:20 +1000

    Tried again on Wk2 Project: still not good enough

--- Wk2/yummy.js
+++ Wk2/yummy.js
@@ -1,14 +1,15 @@
 // Use JS and jQuery to add content to Web Page
 $("#content").prepend("<h1>Welcome to Seafood Restaurant!</h1>");
 $("h1").css("text-align", "center");
-$("dish").click(function() {
-    $("img.#dish").toggle('slow');
-});
-$("table").onfocus = function() {
-    $("#image").append("<img src="./img/location2.jpg" align="absbottom" />");
+$("th#dish").onfocus = function() {
+    $("img.#dish").toggle("show");
+    $("img.#table").toggle("hide");
 };
-//
 
+$("th#table").onfocus = function() {
+    $("img.#table").toggle("show");
+    $("img.#dish").toggle("hide");
+};
 
 //$("li").css("border", "3px, solid, yellow");
 /*
